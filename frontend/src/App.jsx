c95c9d24683a220201b978877f86fb4ea3ecb7fa
Suryakant <surajdwivedi644@gmail.com>
2025-01-11 23:24:23 +0530

    ui building

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css';
+const App = () => {
+  return (
+    <div className='bg-red-50'>
+      <h1 className='bg-red-600 text-lg'>hlo</h1>
+    </div>
+  )
+}
+
+export default App
